f41805172e3c1979ee8909cd910aef6bc3baf0e2
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2023-12-12 10:28:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "GPTs of ChatGPT-4",
+  description: "GPTs of ChatGPT-4 Powerd by Danke.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "GPTs of ChatGPT-4",
     statusBarStyle: "default",
   },
 };
